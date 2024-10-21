7ba1373c0806069bc282121458ba1f2488b06f58
Ryan Bello <ryanbello@gmail.com>
2024-10-21 04:48:21 +0000

    -b2 qclient version

--- foreign/chaos_phil.sh
+++ foreign/chaos_phil.sh
@@ -81,7 +81,7 @@ seqrand ()
 
 qclient ()
 {
-    ./qclient-2.0.1-testnet-linux-amd64 --signature-check=false $* \
+    ./qclient-2.0.1-b2-testnet-linux-amd64 --signature-check=false $* \
         | fgrep -v "Signature check bypassed, be sure you know what you're doing"
 }
 
